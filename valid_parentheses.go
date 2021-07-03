06bdf516cbea59fe2ef9170ef8af40e921309f7b
Alireza Salary <alireza.salary@outlook.com>
2021-07-03 23:36:13 +0430

    add valid-parentheses

---
+++ valid_parentheses.go
@@ -0,0 +1,32 @@
+package main
+
+// https://leetcode.com/problems/valid-parentheses/
+
+func isValid(s string) bool {
+	var (
+		stack    = make([]rune, 0)
+		brackets = map[rune]rune{
+			'(': ')',
+			'{': '}',
+			'[': ']',
+		}
+	)
+
+	for _, r := range s {
+		if r == '(' || r == '{' || r == '[' {
+			stack = append(stack, r)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+
+			if brackets[stack[len(stack)-1]] != r {
+				return false
+			}
+
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
