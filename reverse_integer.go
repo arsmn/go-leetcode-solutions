2f212ff88264907641fbfab26d3c020a7bf090a3
Alireza Salary <alireza.salary@outlook.com>
2021-07-03 18:29:42 +0430

    add reverse-integer

---
+++ reverse_integer.go
@@ -0,0 +1,29 @@
+package main
+
+// https://leetcode.com/problems/reverse-integer/
+
+func reverse(x int) int {
+	var (
+		n, z = 0, 1
+	)
+
+	if x < 0 {
+		z = -1
+	}
+
+	x *= z
+
+	for x > 0 {
+		n *= 10
+		n += x % 10
+		x /= 10
+	}
+
+	n *= z
+
+	if n > 1<<31-1 || n < -1<<31 {
+		return 0
+	}
+
+	return n
+}
