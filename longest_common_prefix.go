d8b0a45247d4048879cfedad84f4eb1baacc46c2
Alireza Salary <alireza.salary@outlook.com>
2021-07-03 20:02:46 +0430

    add longest-common-prefix

---
+++ longest_common_prefix.go
@@ -0,0 +1,19 @@
+package main
+
+// https://leetcode.com/problems/longest-common-prefix/
+
+func longestCommonPrefix(strs []string) string {
+	var prefix string
+
+	for i, r := range strs[0] {
+		for _, str := range strs {
+			runes := []rune(str)
+			if len(runes) <= i || runes[i] != r {
+				return prefix
+			}
+		}
+		prefix += string(r)
+	}
+
+	return prefix
+}
