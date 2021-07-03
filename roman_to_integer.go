0595d0971aefaf825db71dc1bfb1bc1b5f00c931
Alireza Salary <alireza.salary@outlook.com>
2021-07-03 19:29:05 +0430

    add roman-to-integer

---
+++ roman_to_integer.go
@@ -0,0 +1,33 @@
+package main
+
+// https://leetcode.com/problems/roman-to-integer/
+
+func romanToInt(s string) int {
+	var (
+		n     int
+		last  int
+		roman = map[rune]int{
+			'I': 1,
+			'V': 5,
+			'X': 10,
+			'L': 50,
+			'C': 100,
+			'D': 500,
+			'M': 1000,
+		}
+	)
+
+	for _, r := range s {
+		a := roman[r]
+
+		if a <= last {
+			n += a
+		} else {
+			n -= (2 * last) - a
+		}
+
+		last = a
+	}
+
+	return n
+}
