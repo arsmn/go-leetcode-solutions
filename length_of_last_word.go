ecf911d7977fd3b7a8812543400352a27dff99eb
Alireza Salary <alireza.salary@outlook.com>
2021-07-04 14:07:06 +0430

    add length-of-last-word

---
+++ length_of_last_word.go
@@ -0,0 +1,24 @@
+package main
+
+// https://leetcode.com/problems/length-of-last-word/
+
+func lengthOfLastWord(s string) int {
+	var counter, last int
+
+	for _, r := range s {
+		if r == ' ' {
+			if counter > 0 {
+				last = counter
+				counter = 0
+			}
+		} else {
+			counter++
+		}
+	}
+
+	if counter > 0 {
+		last = counter
+	}
+
+	return last
+}
