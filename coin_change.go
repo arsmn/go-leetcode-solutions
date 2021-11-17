77d011a606ae6408c3b9e25fcb631b51af67b9b7
Alireza Salary <alireza.salary@outlook.com>
2021-11-17 13:26:38 +0330

    add coin-change problem

---
+++ coin_change.go
@@ -0,0 +1,24 @@
+package main
+
+import "math"
+
+// https://leetcode.com/problems/coin-change/
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+
+	for i := 1; i <= amount; i++ {
+		dp[i] = math.MaxInt32
+		for _, c := range coins {
+			if i-c >= 0 {
+				dp[i] = minInt(dp[i-c]+1, dp[i])
+			}
+		}
+	}
+
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+
+	return dp[amount]
+}
