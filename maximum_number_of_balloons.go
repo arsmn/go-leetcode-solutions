ec2f31a1879a4830f4625b61711e1ca648ea872f
arsmn <alireza.salary@outlook.com>
2022-08-15 14:05:40 +0300

    add maximum-number-of-balloons

---
+++ maximum_number_of_balloons.go
@@ -0,0 +1,35 @@
+package main
+
+import "math"
+
+// https://leetcode.com/problems/maximum-number-of-balloons/
+
+func maxNumberOfBalloons(text string) int {
+	const word = "balloon"
+
+	wmap := make(map[rune]int)
+	for _, v := range word {
+		wmap[v]++
+	}
+
+	tmap := make(map[rune]int)
+	for _, v := range text {
+		if _, ok := wmap[v]; ok {
+			tmap[v]++
+		}
+	}
+
+	if len(tmap) != len(wmap) {
+		return 0
+	}
+
+	res := math.MaxInt
+
+	for k, v := range wmap {
+		if x := tmap[k] / v; x < res {
+			res = x
+		}
+	}
+
+	return res
+}
