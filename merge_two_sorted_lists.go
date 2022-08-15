56ac8658e54650937865e230eaf5feea46c90b50
arsmn <alireza.salary@outlook.com>
2022-08-15 14:05:53 +0300

    add merge-two-sorted-lists

---
+++ merge_two_sorted_lists.go
@@ -0,0 +1,39 @@
+package main
+
+// https://leetcode.com/problems/merge-two-sorted-lists/
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	var (
+		output = new(ListNode)
+		prev   = output
+	)
+
+	for list1 != nil || list2 != nil {
+		if list1 == nil {
+			prev.Next = list2
+			break
+		}
+
+		if list2 == nil {
+			prev.Next = list1
+			break
+		}
+
+		if list1.Val < list2.Val {
+			prev.Next = list1
+			list1 = list1.Next
+		} else {
+			prev.Next = list2
+			list2 = list2.Next
+		}
+
+		prev = prev.Next
+	}
+
+	return output.Next
+}
