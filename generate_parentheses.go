e28f370e45ecf576947bc330e580df6bbdb90984
arsmn <alireza.salary@outlook.com>
2022-08-15 14:05:12 +0300

    add generate-parentheses

---
+++ generate_parentheses.go
@@ -0,0 +1,21 @@
+package main
+
+import "strings"
+
+// https://leetcode.com/problems/generate-parentheses/
+
+func generateParenthesis(n int) []string {
+	return helper(n, n, "", nil)
+}
+
+func helper(leftLeft, rightLeft int, queue string, res []string) []string {
+	if leftLeft == 0 {
+		return append(res, queue+strings.Repeat(")", rightLeft))
+	}
+
+	if rightLeft > leftLeft {
+		res = helper(leftLeft, rightLeft-1, queue+")", res)
+	}
+
+	return helper(leftLeft-1, rightLeft, queue+"(", res)
+}
